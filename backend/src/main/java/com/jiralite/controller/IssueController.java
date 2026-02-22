438f1f9a93932dec3cc724d674370550878ee1cf
hamidamediaz <amediazhamid736@gmail.com>
2026-02-22 15:17:34 +0100

    add IssueController to test  app

--- backend/src/main/java/com/jiralite/controller/IssueController.java
+++ backend/src/main/java/com/jiralite/controller/IssueController.java
@@ -1,11 +1,17 @@
 package com.jiralite.controller;
 
 import com.jiralite.service.IssueService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/issues")
 public class IssueController {
-    private final IssueService issueService;
 
-    public IssueController(IssueService issueService) {
-        this.issueService = issueService;
+    @GetMapping
+    public ResponseEntity<String> getAll() {
+        return ResponseEntity.ok("liste des issues");
     }
 }
