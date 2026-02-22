b87b8c0f45faee2c016f9ddd85ffad1989455019
hamidamediaz <amediazhamid736@gmail.com>
2026-02-22 13:56:54 +0100

    ci(jenkins): test webhook

--- backend/src/main/java/com/jiralite/controller/IssueController.java
+++ backend/src/main/java/com/jiralite/controller/IssueController.java
@@ -1,4 +1,11 @@
 package com.jiralite.controller;
 
+import com.jiralite.service.IssueService;
+
 public class IssueController {
+    private final IssueService issueService;
+
+    public IssueController(IssueService issueService) {
+        this.issueService = issueService;
+    }
 }
